730f1baf455e0689af53045780ef2c757fa2b5f0
Marcin Szostak <m.szostak2907@icloud.com>
2023-12-06 11:57:07 +0100

    Prepared Maven project structure and added README

---
+++ README.md
@@ -0,0 +1,2 @@
+# Expression evaluator
+Demo project - it implements reverse Polish notation (postfix notation) to evaluate arithmetic expressions presented as a string.
---
+++ src/test/java/pl/edu/zut/expression_evaluator/EvaluatorTestRunTest.java
@@ -0,0 +1,12 @@
+package pl.edu.zut.expression_evaluator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class EvaluatorTestRunTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
