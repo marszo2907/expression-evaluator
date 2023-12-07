cc9e6a3e13f49cc74428890f2db02efe1bf7bcf9
Marcin Szostak <m.szostak2907@icloud.com>
2023-12-07 09:18:31 +0100

    Updated dependencies in the pom.xml file and provided a test infrastructure for happy day addition scenario

--- src/test/java/pl/edu/zut/expression_evaluator/EvaluatorTestRunTest.java
+++ src/test/java/pl/edu/zut/expression_evaluator/EvaluatorTestRunTest.java
@@ -1,8 +1,8 @@
 package pl.edu.zut.expression_evaluator;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class EvaluatorTestRunTest {
     @Test
--- src/test/java/pl/edu/zut/expression_evaluator/postfix_notation_evaluator/PostfixEvaluatorTest.java
+++ src/test/java/pl/edu/zut/expression_evaluator/postfix_notation_evaluator/PostfixEvaluatorTest.java
@@ -1,7 +1,108 @@
 package pl.edu.zut.expression_evaluator.postfix_notation_evaluator;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.ParameterizedTest;
+import pl.edu.zut.expression_evaluator.postfix_notation_evaluator.PostfixEvaluator;
 
 public class PostfixEvaluatorTest {
-    
+    private static PostfixEvaluator postfixEvaluator;
+
+    @BeforeAll
+    static void init() {
+        postfixEvaluator = PostfixEvaluator.getInstance();
+    }
+
+    @ParameterizedTest
+    @MethodSource("validAdditionTokensProvider")
+    public void testValidAddtiton(List<String> tokens, String expectedResultStr) {
+        BigDecimal expectedResult = new BigDecimal(expectedResultStr);
+
+        BigDecimal result = postfixEvaluator.evaluate(tokens);
+
+        assertEquals(expectedResult, result);
+    }
+
+    private static Stream<Arguments> validAdditionTokensProvider() {
+        return Stream.of(
+            Arguments.of(
+                List.of("0", "0", "+"),
+                "0"
+            ),
+            Arguments.of(
+                List.of("0", "1", "+"),
+                "1"
+            ),
+            Arguments.of(
+                List.of("1", "0", "+"),
+                "1"
+            ),
+            Arguments.of(
+                List.of("1", "2", "+"),
+                "3"
+            ),
+            Arguments.of(
+                List.of("2", "1", "+"),
+                "3"
+            ),
+            Arguments.of(
+                List.of("-1", "2", "+"),
+                "1"
+            ),
+            Arguments.of(
+                List.of("2", "-1", "+"),
+                "1"
+            ),
+            Arguments.of(
+                List.of("-1", "-2", "+"),
+                "-3"
+            ),
+            Arguments.of(
+                List.of("-2", "-1", "+"),
+                "-3"
+            ),
+            Arguments.of(
+                List.of("0", "0", "+", "1", "+"),
+                "1"
+            ),
+            Arguments.of(
+                List.of("0", "1", "+", "2", "+"),
+                "3"
+            ),
+            Arguments.of(
+                List.of("1", "0", "+", "2", "+"),
+                "3"
+            ),
+            Arguments.of(
+                List.of("1", "2", "+", "3", "+"),
+                "6"
+            ),
+            Arguments.of(
+                List.of("2", "1", "+", "3", "+"),
+                "6"
+            ),
+            Arguments.of(
+                List.of("-1", "2", "+", "-3", "+"),
+                "-2"
+            ),
+            Arguments.of(
+                List.of("2", "-1", "+", "-3", "+"),
+                "-2"
+            ),
+            Arguments.of(
+                List.of("-1", "-2", "+", "-3", "+"),
+                "-6"
+            ),
+            Arguments.of(
+                List.of("-2", "-1", "+", "-3", "+"),
+                "-6"
+            )
+        );
+    }
 }
